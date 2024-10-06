1d8155564a79f96ce6f6c4f709b4fc79e25a50ee
Devin Barry <devinbarry@users.noreply.github.com>
2024-10-06 04:23:25 +0100

    Add simple CustomersTable.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function CustomersPage(
+  props: {
+    searchParams?: Promise<{
+      query?: string;
+      page?: string;
+    }>;
+  }
+) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <CustomersTable customers={customers}/>
+    </Suspense>
+  )
 }
